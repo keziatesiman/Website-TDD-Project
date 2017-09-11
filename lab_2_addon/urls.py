697fe43e96539650c6fc6ede857335ba0c71c4e3
keziatesiman <irenekezia98@gmail.com>
2017-09-11 20:49:30 +0700

    lab 2 addon

---
+++ lab_2_addon/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from .views import index
+
+#url for app, add your URL Configuration
+
+urlpatterns = [
+#TODO Implement this
+    url(r'^$', index, name='index'),
+]
---
+++ lab_2_addon/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from lab_1.views import mhs_name, birth_date
+    #Create a list of biodata that you wanna show on webpage:
+    #[{'subject' : 'Name', 'value' : 'Igor'},{{'subject' : 'Birth Date', 'value' : '11 August 1970'},{{'subject' : 'Sex', 'value' : 'Male'}
+    #TODO Implement
+bio_dict = [{'subject' : 'Name', 'value' : mhs_name},\
+    {'subject' : 'Birth Date', 'value' : birth_date.strftime('%d %B %Y')},\
+    {'subject' : 'Sex', 'value' : 'Female'}]
+
+def index(request):
+    response = {'bio_dict' : bio_dict}
+    return render(request, 'description_lab2addon.html', response)
