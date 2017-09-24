086a137388fedae0a809e80c1e0179cb0dd84ef9
keziatesiman <irenekezia98@gmail.com>
2017-09-24 22:53:30 +0700

    supposed to be passed test3

--- lab_4/urls.py
+++ lab_4/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from .views import index
-
+from .views import index, add_message
 
 #url for app, add your URL Configuration
 
 urlpatterns = [
 #TODO Implement this
 	url(r'^$', index, name='index'),
+	url(r'^add_message', message_post, name='add_message'),
 
 ]
--- lab_4/views.py
+++ lab_4/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import Message_Form
+from .models import Message
+
 
 from lab_2.views import landing_page_content
 
@@ -11,5 +15,23 @@ def index(request):
     #TODO Implement, isilah dengan 6 kata yang mendeskripsikan anda
     response['about_me'] = about_me
     return render(request, html, response)
+
+    response['message_form'] = Message_Form
+    return render(request, html, response)
+
+def message_post(request):
+    form = Message_Form(request.POST or None)
+    if(request.method == 'POST' and form.is_valid()):
+        response['name'] = request.POST['name'] if request.POST['name'] != "" else "Anonymous"
+        response['email'] = request.POST['email'] if request.POST['email'] != "" else "Anonymous"
+        response['message'] = request.POST['message']
+        message = Message(name=response['name'], email=response['email'],
+                          message=response['message'])
+        message.save()
+        html ='lab_4/form_result.html'
+        return render(request, html, response)
+    else:        
+        return HttpResponseRedirect('/lab-4/')
+
 
 # Create your views here.
