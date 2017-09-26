d0eaffe5f98e9026072b4d5fd85e2b93e1ef44e3
keziatesiman <irenekezia98@gmail.com>
2017-09-26 12:55:21 +0700

    fixing timezone

--- lab_4/models.py
+++ lab_4/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Message(models.Model):
+    def convertTimezone():
+        return timezone.now() + timezone.timedelta(hours=7)
+
+
     name = models.CharField(max_length=27)
     email = models.EmailField()
     message = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(default=convertTimezone)
 
     def __str__(self):
         return self.message
