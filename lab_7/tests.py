ad2756f65531c9842a47393f686429fe8a7529e7
keziatesiman <irenekezia98@gmail.com>
2017-11-14 14:59:45 +0700

    test repair 2

--- lab_7/tests.py
+++ lab_7/tests.py
@@ -20,19 +20,19 @@ class Lab7UnitTest(TestCase):
 		self.assertTemplateUsed(response, 'lab_7/lab_7.html')
 
 	def test_lab7_using_daftar_teman_template(self):
-		response = Client().get('/lab-7/friend-list')
+		response = Client().get('/lab-7/friend-list/')
 		self.assertTemplateUsed(response, 'lab_7/daftar_teman.html')
 
 	def test_lab7_using_friend_list_function(self):
-		found = resolve('/lab-7/friend-list')
+		found = resolve('/lab-7/friend-list/')
 		self.assertEqual(found.func, friend_list)
 
 	def test_lab7_using_get_friend_list_funct(self):
-		found = resolve('/lab-7/get-friend-list')
+		found = resolve('/lab-7/get-friend-list/')
 		self.assertEqual(found.func, get_friend_list)
 
 	def test_lab7_using_add_friend_funct(self):
-		found = resolve('/lab-7/friend-list')
+		found = resolve('/lab-7/add-friend/')
 		self.assertEqual(found.func, add_friend)
 
 	def test_model_can_create_new_friend(self):
